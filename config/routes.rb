c4ce87422bdb933e5a1e609458ea01403104b641
Nishi12Y <nishimura.y12@gmail.com>
2025-12-13 17:20:07 +0900

    add:topページへの遷移を追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "static_pages#top"
 
   resources :groups, param: :uuid, only: [ :new, :create, :show ] do
     member do
