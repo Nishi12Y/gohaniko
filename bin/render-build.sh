594091fa1e56077657b67695a61486344fadf4cf
Nishi12Y <nishimura.y12@gmail.com>
2025-12-14 10:48:38 +0900

    fix:renderデプロイ用の修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,9 @@
 set -o errexit
 
 bundle install
-yarn install
-yarn build:css
+npm install
+npm run build
+npm run build:css
 bundle exec rails assets:precompile
-bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed
