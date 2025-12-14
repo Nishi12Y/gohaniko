cc92e372394971a745b764582a967bc8ab80d6df
Nishi12Y <nishimura.y12@gmail.com>
2025-12-14 12:22:29 +0900

    Revert "fix:renderデプロイ用の修正"
    This reverts commit 594091fa1e56077657b67695a61486344fadf4cf.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,6 @@ Rails.application.configure do
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # esbuild の出力先をアセットパスに含める
-  config.assets.paths << Rails.root.join("app/assets/builds")
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
