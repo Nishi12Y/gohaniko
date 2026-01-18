53abb3af59d7e7d890ffee53a4f7d6224eb788f3
Nishi12Y <nishimura.y12@gmail.com>
2026-01-18 11:45:19 +0900

    add:ユーザーのスケジュール登録機能追加

--- app/controllers/user_schedules_controller.rb
+++ app/controllers/user_schedules_controller.rb
@@ -4,4 +4,51 @@ class UserSchedulesController < ApplicationController
     @group = Group.find_by(uuid: params[:group_uuid])
     @group_schedule_dates = @group.group_schedule_dates.order(:date)
   end
+
+  def create
+    @group = Group.find_by(uuid: params[:group_uuid])
+    ActiveRecord::Base.transaction do
+      # ① schedule_participant 作成 or 取得
+      participant = ScheduleParticipant.create!(
+        group_id: @group.id,
+        **schedule_participant_params
+      )
+      puts ("schedule_params:" + schedules_params.inspect)
+
+      # ② user_schedules 用データを組み立て
+      now = Time.current
+      records = schedules_params.map do |schedule|
+        {
+          group_schedule_date_id: schedule[:group_schedule_date_id],
+          schedule_participant_id: participant.id,
+          choice: schedule[:choice],
+          created_at: now,
+          updated_at: now
+        }
+      end
+
+      # ③ 複数日付を一括保存（重複は更新）
+      UserSchedule.upsert_all(
+        records,
+        unique_by: :index_user_schedules_on_date_and_participant
+      )
+    end
+
+    redirect_to group_group_schedule_dates_path(@group), notice: "出欠を登録しました"
+  rescue ActiveRecord::RecordInvalid => e
+    flash.now[:alert] = "登録に失敗しました：#{e.record.errors.full_messages.join('、')}"
+    render :new, status: :unprocessable_entity
+  end
+
+  private
+
+  def schedule_participant_params
+    params.require(:schedule_participant).permit(:name)
+  end
+
+  def schedules_params
+    params.require(:schedules).values.map do |p|
+      p.permit(:group_schedule_date_id, :choice)
+    end
+  end
 end
