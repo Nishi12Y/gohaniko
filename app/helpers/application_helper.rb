065a2f3d220be8a8c4b22f839b6bd286355723cb
Nishi12Y <nishimura.y12@gmail.com>
2026-02-07 16:40:13 +0900

    fix:lintチェックによる修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,27 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'ごはんいこ！',
-      title: 'ごはんいこ！ - みんなでご飯の予定を簡単に調整',
+      site: "ごはんいこ！",
+      title: "ごはんいこ！ - みんなでご飯の予定を簡単に調整",
       reverse: true,
-      charset: 'utf-8',
-      description: 'ごはんいこ！では、スケジュールの調整や、みんなでご飯の候補を決めたり、言いにくいことを匿名で共有できたりします。',
-      keywords: 'ご飯,共有',
-      canonical: 'https://gohaniko.onrender.com/',
-      separator: '|',
-      og:{
+      charset: "utf-8",
+      description: "ごはんいこ！では、スケジュールの調整や、みんなでご飯の候補を決めたり、言いにくいことを匿名で共有できたりします。",
+      keywords: "ご飯,共有",
+      canonical: "https://gohaniko.onrender.com/",
+      separator: "|",
+      og: {
         site_name: :site,
         title: :title,
         description: :description,
-        type: 'website',
-        url: 'https://gohaniko.onrender.com/',
-        image: image_url('gohaniko.png'),
-        local: 'ja-JP'
+        type: "website",
+        url: "https://gohaniko.onrender.com/",
+        image: image_url("gohaniko.png"),
+        local: "ja-JP"
       },
       twitter: {
-        card: 'summary_large_image',
-        site: '@obvyamdrss',
-        image: image_url('gohaniko.png')
+        card: "summary_large_image",
+        site: "@obvyamdrss",
+        image: image_url("gohaniko.png")
       }
     }
   end
