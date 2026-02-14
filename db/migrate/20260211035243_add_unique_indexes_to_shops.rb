af71ed62e574d96bf6e1e8b3eaca1a1f68fc1e40
Nishi12Y <nishimura.y12@gmail.com>
2026-02-14 10:41:21 +0900

    fix:lintチェック修正

--- db/migrate/20260211035243_add_unique_indexes_to_shops.rb
+++ db/migrate/20260211035243_add_unique_indexes_to_shops.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexesToShops < ActiveRecord::Migration[7.2]
   def change
-    add_index :shops, [:group_id, :url],  unique: true
+    add_index :shops, [ :group_id, :url ],  unique: true
   end
 end
